b401881e53d4c43fe33cc9c7b3e34e04a48a6caa
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-10-08 12:09:57 +0200

    second check

---
+++ pre_commit_hooks/team/team.py
@@ -0,0 +1,19 @@
+import sys
+from .utils import get_frontmatter
+
+
+def main():
+    VALID_TEAMS = ["Writers", "Tech", "Marketing"]
+    post = get_frontmatter()
+    teams: list[str] = post.get("team", None)
+    if(teams is None or not isinstance(teams, list) or len(teams) == 0):
+        print("team must be a non-empty array")
+        sys.exit(1)
+    mismatch = []
+    for team in teams:
+        if team not in VALID_TEAMS:
+            mismatch.append(team)
+    if(len(mismatch) > 0):
+        print(f"Invalid team name(s): {','.join(mismatch)}")
+        sys.exit(1)
+    sys.exit(0) // ok
