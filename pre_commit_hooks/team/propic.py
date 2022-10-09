d2e60e9645ac7e679c59206bc1d15bb685c699ac
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-10-09 17:55:13 +0200

    finished team checks (for real)

--- pre_commit_hooks/team/propic.py
+++ pre_commit_hooks/team/propic.py
@@ -1,9 +1,15 @@
-from .utils import filepath, print_error
+from .utils import filepath, print_error, filename
 import imageio
+import sys
 
 
 def main():
+    if filename != "propic.jpg":
+        print_error("Propic filename must be propic.jpg")
+        return sys.exit(1)
     img = imageio.imread(filepath)
     if img.shape[0] != img.shape[1]:
         print_error(
             f"Team member propic is not a square ({img.shape[0]}x{img.shape[1]})")
+        return sys.exit(1)
+    sys.exit(0)
