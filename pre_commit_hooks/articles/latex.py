3640836c6d93116f7a492afb3e6d0c5a04e7867b
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-10-09 20:27:46 +0200

    fix latex

--- pre_commit_hooks/articles/latex.py
+++ pre_commit_hooks/articles/latex.py
@@ -3,15 +3,16 @@ from .utils import get_frontmatter, print_error
 from urllib.parse import urlparse
 # ![Rete Generativa Avversaria](./Untitled.png)
 
-eq = r"(\$+)(?:(?!\1)[\s\S])*\1"
+eq_pattern = r"(\$+)(?:(?!\1)[\s\S])*\1"
 
 def main():
     post = get_frontmatter()
     content = post.content
-    eqs = list(re.finditer(eq, content))
+    eqs = list(re.finditer(eq_pattern, content))
     malformed_eqs = []
-    for eq in [eq for eq in eqs if eq.group(0).startswith("$$")]:
-        if not (eq.group(0).startswith("$$\n") and eq.group(0).endswith("\n$$")):
+    for eq in [e for e in eqs if e.group(0).startswith("$$")]:
+        equation = eq.group(0)
+        if not (equation.startswith("$$\n") and re.search(r"\n[ \t]*\$+$", equation)):
             malformed_eqs.append((eq.group(0), eq.span()))
     if len(malformed_eqs) > 0:
         print_error(f"The following equations are not valid: {malformed_eqs} ", True)
