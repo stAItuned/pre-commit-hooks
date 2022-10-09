0a68cfefbf364a802b480fa5bfb17e0b5cc2b231
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-10-09 18:42:23 +0200

    bugfix

--- pre_commit_hooks/articles/author.py
+++ pre_commit_hooks/articles/author.py
@@ -6,21 +6,22 @@ import frontmatter
 
 
 def get_all_authors() -> list[str]:
-    team_path = path.abspath(path.join(filepath, "..", "..", "team"))
-    authors_filename = glob.glob("*/*.md", team_path)
+    team_path = path.abspath(path.join(path.dirname(filepath), "..", "..", "team"))
+    print(team_path)
+    authors_filename = [path.join(team_path, a) for a in glob.glob("*/*.md", root_dir=team_path)]
     authors_names = []
     for fn in authors_filename:
         with open(fn, "r") as f:
             p = frontmatter.loads(f.read())
             authors_names.append(p.get("name"))
-    return author_names
+    return authors_names
 
 
 def main():
     post = get_frontmatter()
     author: str = post.get("author", "")
     author_names = get_all_authors()
-    if not author in author_names == 0:
+    if author not in author_names:
         print_error(
             f"Author must exists, {author} doesn't. (List of valid authors: {sorted(author_names)})")
         return sys.exit(1)
