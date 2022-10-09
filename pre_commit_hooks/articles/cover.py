a2bace7b62679568588693d5f6a1d17b7dffc9d3
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-10-09 18:36:26 +0200

    bugfix

--- pre_commit_hooks/articles/cover.py
+++ pre_commit_hooks/articles/cover.py
@@ -2,7 +2,6 @@ from genericpath import isfile
 import sys
 from .utils import get_frontmatter, print_error, filepath
 from os import path
-from os import isfile
 from identify.identify import tags_from_filename
 
 def main():
@@ -10,7 +9,7 @@ def main():
     cover: str = post.get("cover", "")
     cover_path = path.join(path.dirname(filepath), cover)
 
-    if not isfile(cover_path) or "image" not in tags_from_filename(cover_path):
+    if "image" not in tags_from_filename(cover_path):
         print_error(f"Cover not valid, must be a valid image inside {path.dirname(filepath)} folder")
         return sys.exit(1)
     sys.exit(0)
