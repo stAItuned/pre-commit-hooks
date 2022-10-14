296f335a911f63c5c29abad9a5358576dfa659ca
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-10-14 10:25:00 +0200

    prettified output on frontmatter error

--- pre_commit_hooks/articles/utils.py
+++ pre_commit_hooks/articles/utils.py
@@ -1,22 +1,26 @@
 import frontmatter
 import sys
 from os import path
-
+from yaml.scanner import ScannerError
 BASE_PATH = path.abspath(".")
 filename = sys.argv[1]
 filepath = path.join(BASE_PATH, filename)
 
 
 def get_frontmatter():
-    with open(filepath) as f:
-        post = frontmatter.loads(f.read())
-    return post
+    try:
+        with open(filepath) as f:
+            post = frontmatter.loads(f.read())
+        return post
+    except ScannerError:
+        print_error("Frontmatter not well-written.", True)
 
 
 def print_error(msg: str, exit: bool = False):
     print(f"{path.relpath(filepath)}... {msg}")
     if exit:
         sys.exit(1)
+
 
 def ok(msg: str = "OK", exit: bool = True):
     print(f"{path.relpath(filepath)}... {msg}")
--- pre_commit_hooks/team/utils.py
+++ pre_commit_hooks/team/utils.py
@@ -1,6 +1,7 @@
 import frontmatter
 import sys
 from os import path
+from yaml.scanner import ScannerError
 
 BASE_PATH = path.abspath(".")
 filename = sys.argv[1]
@@ -8,10 +9,12 @@ filepath = path.join(BASE_PATH, filename)
 
 
 def get_frontmatter():
-    with open(filepath) as f:
-        post = frontmatter.loads(f.read())
-    return post
-
+    try:
+        with open(filepath) as f:
+            post = frontmatter.loads(f.read())
+        return post
+    except ScannerError:
+        print_error("Frontmatter not well-written.", True)
 
 def print_error(msg: str, exit: bool = False):
     print(f"{path.relpath(filepath)}... {msg}")
