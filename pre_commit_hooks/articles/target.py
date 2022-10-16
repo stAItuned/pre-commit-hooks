e2adebf341790b18dc5b9c933bc257aeaf0e5c71
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-10-16 17:02:15 +0200

    fix stupid mistake

--- pre_commit_hooks/articles/target.py
+++ pre_commit_hooks/articles/target.py
@@ -7,7 +7,7 @@ ERROR_MSG = f"Target not valid, must be one of the following: {VALID_TARGETS}"
 def main():
     post = get_frontmatter()
     target: str = post.get("target", "")
-    if target.capitalize() in VALID_TARGETS:
+    if target.lower() == target and target.capitalize() in VALID_TARGETS:
         rewrite_frontmatter_property(post, "target", target.capitalize())
         return print_error(f"(AUTOFIXED) {ERROR_MSG}", True)
     if not target in VALID_TARGETS:
--- pre_commit_hooks/team/propic.py
+++ pre_commit_hooks/team/propic.py
@@ -4,15 +4,15 @@ from os import rename
 
 
 def main():
+    if not filename.endswith("propic.jpg") and not filename.endswith("propic.jpeg"):
+        return print_error(f"Propic filename must be propic.jpg", True)
+
     if filename.endswith("propic.jpeg"):
         new_name = filename.replace("propic.jpg")
         rename(filename, new_name)
-        filename = new_name
         ok("(AUTOFIX) Propic renamed from .jpeg to .jpg", False)
 
-    if not filename.endswith("propic.jpg"):
-        return print_error(f"Propic filename must be propic.jpg", True)
-    img = imageio.imread(filepath)
+    img = imageio.imread(filepath.replace("propic.jpeg", "propic.jpg"))
     if img.shape[0] != img.shape[1]:
         return print_error(
             f"Team member propic is not a square ({img.shape[0]}x{img.shape[1]})", True)
