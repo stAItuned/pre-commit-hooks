795f5d848f92e2b4a20690bda37418b0074472c1
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-10-18 10:37:41 +0200

    changed team description length max

--- pre_commit_hooks/team/description.py
+++ pre_commit_hooks/team/description.py
@@ -1,12 +1,12 @@
 from typing import Literal
 from ..utils import Entry, get_frontmatter, ok, print_error
 
-DESCRIPTION_MAX_LEN = 200
+DESCRIPTION_MAX_LEN = 500
 
 
 def single_entry(entry: Entry) -> Literal[1, 0]:
     post = entry.post
-    description: str = post.get("description", "")
+    description: str = post.get("description", "").replace(" ", "")
     if len(description) > DESCRIPTION_MAX_LEN:
         return print_error(entry, f"Description too long, {len(description)}/{DESCRIPTION_MAX_LEN} chars", True)
     return ok(entry)
